48a321e3ab0ffbaebd63ba4723a4268e28e61713
Houston Davis <hdavis.147@gmail.com>
2023-08-24 14:35:54 -0500

    Fixed missing comma in employees table

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,14 @@ CREATE DATABASE directory_db;
 USE directory_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
-  salary DECIMAL NOT NULL,
+  salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES departments(id)
@@ -19,13 +19,15 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES roles(id),
+  REFERENCES roles(id)
+  ON DELETE SET NULL,
   FOREIGN KEY (manager_id)
   REFERENCES employees(id)
-)
+  ON DELETE SET NULL
+);
